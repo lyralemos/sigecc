0d03717d244a56014a36d12cf9f30d2d88467915
Alexandre Marinho <lyralemos@gmail.com>
2019-05-20 09:48:58 -0300

    Informação sobre finalizado

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import *
 
 class ModuloAdmin(admin.ModelAdmin):
-    list_display = ('__str__','colaboracao', 'competicao', 'ativo', 'liberado')
+    list_display = ('__str__','colaboracao', 'competicao', 'ativo', 'liberado', 'finalizado')
 
 
 class QuestaoAdmin(admin.ModelAdmin):
